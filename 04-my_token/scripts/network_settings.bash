5d41c1fdd875e0509be4d8bab35a7d5fbeee7e1d
asiroya8 <97836817+asiroya8@users.noreply.github.com>
2022-01-18 20:33:11 +0530

    Update network_settings.bash

--- 04-my_token/scripts/network_settings.bash
+++ 04-my_token/scripts/network_settings.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-brownie networks add Ethereum ropsten_test host='YOUR LINK' chainid=3 explorer='https://api-ropsten.exit'
+brownie networks add Ethereum ropsten_test host=https://speedy-nodes-nyc.moralis.io/d5b0b06985d6ce1bdb84063a/eth/ropsten   chainid=3 explorer='https://api-ropsten.exit'
